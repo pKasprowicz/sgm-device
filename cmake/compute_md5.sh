568f9e43f06af61cdd3d04e5008ebdecfe5c40de
root <root@vps295754.ovh.net>
2016-10-16 14:16:54 +0200

    Build system tuned

--- cmake/compute_md5.sh
+++ cmake/compute_md5.sh
@@ -3,6 +3,6 @@
 WC=$(pwd)
 
 cd ${1}
-find  -L ./ -type f -exec md5sum {} \; | md5sum | cut -d' ' -f1 > ${WC}/sgmeteo.md5
+find  -L ./ -type f -exec md5sum {} \; | md5sum | cut -d' ' -f1 > ${WC}/install.md5
 echo MD5: $(cat ${WC}/install.md5)
 cd ${WC}
